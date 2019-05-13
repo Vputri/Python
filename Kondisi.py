516f0f1823a9c3a80e7ef9f74469d85c9d72fa05
Vputri <35414466+Vputri@users.noreply.github.com>
2019-05-13 20:37:30 +0700

    Python

---
+++ Kondisi.py
@@ -0,0 +1,27 @@
+print '                                  Menu Makanan'
+print ''
+print '                               =================== '
+print '                              |     PILIHAN :     |'
+print '                              |                   |'
+print '                              | 1.Nasi Goreng     |'
+print '                              | 2.Nasi Rames      |'
+print '                              | 3.Sop Iga         |'
+print '                              | 4.Gado-Gado       |'
+print '                              |                   |'
+print '                               ==================='
+print ''
+
+x = (raw_input("Masukkan Pilihan (1-4) : "))
+print ''
+
+if x == "1":
+    print 'Nasi Goreng'
+ 
+elif x == "2":
+    print 'Nasi Rames'
+
+elif x == "3":
+    print 'Sop Iga'
+ 
+elif x == "4":
+    print 'Gado-Gado'
